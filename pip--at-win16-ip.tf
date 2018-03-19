7662aec9eae6f32898e4f51fd83a204fe88c3b71
andyt530 <andyt530@outlook.com>
2018-03-19 21:09:17 +0000

    driver added 2

--- pip--at-win16-ip.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_public_ip" "at-win16-ip" {
-	 name = "at-win16-ip"
-	 location = "${var.loctarget}"
-	 resource_group_name = "${var.rgtarget}"
-	 public_ip_address_allocation = "Dynamic" 
-}
