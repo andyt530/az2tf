fe8e13a7f51729b974abc14efe4b1cb2144bd484
andyt530 <andyt530@outlook.com>
2018-04-09 20:37:12 +0100

    az2tf

--- az2tf.sh
+++ az2tf.sh
@@ -1,10 +1,10 @@
 if [ "$1" != "" ]; then
-    myrg=$1
+    mysub=$1
 else
-    echo -n "Enter name of Resource Group [$rgsource] > "
+    echo -n "Enter id of Subscription [$mysub] > "
     read response
     if [ -n "$response" ]; then
-        myrg=$response
+        mysub=$response
     fi
 fi
 comm[1]="./scripts/01-azurerm_resource_group.sh"
@@ -12,7 +12,7 @@ comm[2]="./scripts/03-azurerm_route_table.sh"
 comm[6]="./scripts/02-azurerm_availability_set.sh $myrg"
 comm[5]="./scripts/04-azurerm_network_security_group.sh $myrg"
 comm[3]="./scripts/06-azurerm_subnet.sh $myrg"
-comm[4]="./scripts/08-azure_virtual_network.sh $myrg"
+comm[4]="./scripts/08-azurerm_virtual_network.sh $myrg"
 comm[7]="./scripts/11-azurerm_managed_disk.sh $myrg"
 comm[8]="./scripts/12-azurerm_storage_account.sh $myrg"
 comm[9]="./scripts/14-azurerm_public_ip.sh $myrg"
@@ -24,6 +24,7 @@ export ARM_CLIENT_SECRET=""
 export ARM_TENANT_ID=""
 export ARM_SUBSCRIPTION_ID=""
 export TF_VAR_rgtarget=$myrg
+az account set -s $mysub
 #echo "Clean Dir"
 #./cleanup.sh
 #echo "Clean terraform"
@@ -31,7 +32,7 @@ export TF_VAR_rgtarget=$myrg
 #az account set -s $ARM_SUBSCRIPTION_ID
 rm terraform*.backup
 cp stub/*.tf .
-for j in `seq 1 3`; do
+for j in `seq 11 11`; do      # 7 - managed disk - needs work
     echo $i
     trgs=`az group list`
     count=`echo $trgs | jq '. | length'`
