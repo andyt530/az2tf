338749fe1035d6fc8f95d4ba37797880aeee071b
andyt530 <andyt530@outlook.com>
2018-04-20 14:45:13 +0100

    avail set fixes

--- az2tf.sh
+++ az2tf.sh
@@ -10,6 +10,7 @@ fi
 
 mkdir -p tf.$mysub
 cd tf.$mysub
+
 pfx[1]="rg"
 res[1]="azurerm_resource_group"
 pfx[2]="rtb"
@@ -38,21 +39,13 @@ pfx[13]="lb"
 res[13]="azurerm_lb"
 pfx[14]="vm"
 res[14]="azurerm_virtual_machine"
+pfx[15]="lck"
+res[15]="azurerm_management_lock"
 
-comm[1]="../scripts/${res[1]}.sh"
-comm[2]="../scripts/${res[2]}.sh"
-comm[3]="../scripts/${res[3]}.sh $myrg"
-comm[4]="../scripts/${res[4]}.sh $myrg"
-comm[5]="../scripts/${res[5]}.sh $myrg"
-comm[6]="../scripts/${res[6]}.sh $myrg"
-comm[7]="../scripts/${res[7]}.sh $myrg"
-comm[8]="../scripts/${res[8]}.sh $myrg"
-comm[9]="../scripts/${res[9]}.sh $myrg"
-comm[10]="../scripts/${res[10]}.sh $myrg"
-comm[11]="../scripts/${res[11]}.sh $myrg"
-comm[12]="../scripts/${res[12]}.sh $myrg"
-comm[13]="../scripts/${res[13]}.sh $myrg"
-comm[14]="../scripts/${res[14]}.sh $myrg"
+pfx[51]="rdf"
+res[51]="azurerm_role_definition"
+pfx[52]="ras"
+res[52]="azurerm_role_assignment"
 
 source ../setup-vars.sh
 export ARM_SUBSCRIPTION_ID=""
@@ -68,16 +61,22 @@ az account set -s $mysub
 #az account set -s $ARM_SUBSCRIPTION_ID
 
 rm terraform*.backup
+rm tf*.sh
 cp ../stub/*.tf .
 echo "init"
 terraform init
 
-for j in `seq 1 14`; do  
-    
-    for t in `terraform state list | grep azurerm_availability_set`
-        do
-t       erraform state rm $i
-    done
+
+for j in `seq 51 52`; do
+
+docomm="../scripts/${res[$j]}.sh $mysub"
+    echo $docomm
+    eval $docomm
+done
+
+
+for j in `seq 1 15`; do  
+
 
     trgs=`az group list`
     count=`echo $trgs | jq '. | length'`
