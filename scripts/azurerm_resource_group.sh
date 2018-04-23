b3b6f623b3df0ddba2722c79570b0f93f5001431
andyt530 <andyt530@outlook.com>
2018-04-23 18:08:00 +0100

    rg handles tags better

--- scripts/azurerm_resource_group.sh
+++ scripts/azurerm_resource_group.sh
@@ -29,16 +29,17 @@ tags=`echo $azr | jq ".tags"`
 tt=`echo $tags | jq .`
 tcount=`echo $tags | jq '. | length'`
 if [ "$tcount" -gt "0" ]; then
-    printf "\t tags { \n" >> $prefix-$name.tf
+    printf "\t tags { \n" >> $prefix.tf
     tt=`echo $tags | jq .`
-    for j in `seq 1 $tcount`; do
-        atag=`echo $tt | cut -d',' -f$j | tr -d '{' | tr -d '}'`
-        tkey=`echo $atag | cut -d':' -f1 | tr -d '"'`
-        tval=`echo $atag | awk -F '": ' '{print $2}' | tr -d '"'`
-        printf "\t\t%s = \"%s\" \n" $tkey $tval >> $prefix-$name.tf
-        
+    keys=`echo $tags | jq 'keys'`
+    tcount=`expr $tcount - 1`
+    for j in `seq 0 $tcount`; do
+        k1=`echo $keys | jq ".[(${j})]"`
+        tval=`echo $tt | jq .$k1`
+        tkey=`echo $k1 | tr -d '"'`
+        printf "\t\t%s = %s \n" $tkey "$tval" >> $prefix.tf
     done
-    printf "\t}\n" >> $prefix-$name.tf
+    printf "\t}\n" >> $prefix.tf
 fi
 
 
