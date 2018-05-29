e42d9bdffefdad406823eb3968417bec99dfac25
andyt530 <andyt530@outlook.com>
2018-05-29 23:07:08 +0100

    vng cut 1

--- scripts/azurerm_virtual_network_gateway.sh
+++ scripts/azurerm_virtual_network_gateway.sh
@@ -1,5 +1,5 @@
-tfp="azurerm_local_network_gateway"
-prefixa="lng"
+tfp="azurerm_virtual_network_gateway"
+prefixa="vng"
 if [ "$1" != "" ]; then
     rgsource=$1
 else
@@ -9,7 +9,7 @@ else
         rgsource=$response
     fi
 fi
-azr=`az network local-gateway list -g $rgsource`
+azr=`az network vnet-gateway list -g $rgsource`
 count=`echo $azr | jq '. | length'`
 if [ "$count" -gt "0" ]; then
     count=`expr $count - 1`
@@ -18,29 +18,60 @@ if [ "$count" -gt "0" ]; then
         rg=`echo $azr | jq ".[(${i})].resourceGroup" | tr -d '"'`
         id=`echo $azr | jq ".[(${i})].id" | tr -d '"'`
         loc=`echo $azr | jq ".[(${i})].location" | tr -d '"'`
-        gwaddr=`echo $azr | jq ".[(${i})].gatewayIpAddress" | tr -d '"'`
-        addrpre=`echo $azr | jq ".[(${i})].localNetworkAddressSpace.addressPrefixes"`
+        type=`echo $azr | jq ".[(${i})].gatewayType" | tr -d '"'`
+        vpntype=`echo $azr | jq ".[(${i})].vpnType" | tr -d '"'`
         bgps=`echo $azr | jq ".[(${i})].bgpSettings" | tr -d '"'`
+        sku=`echo $azr | jq ".[(${i})].sku.name" | tr -d '"'`
+
+        aa=`echo $azr | jq ".[(${i})].activeActive"`
+        enbgp=`echo $azr | jq ".[(${i})].enableBgp"`
         prefix=`printf "%s__%s" $prefixa $rg`
         
         printf "resource \"%s\" \"%s__%s\" {\n" $tfp $rg $name > $prefix-$name.tf
         printf "\t name = \"%s\"\n" $name >> $prefix-$name.tf
         printf "\t resource_group_name = \"%s\"\n" $rg >> $prefix-$name.tf
         printf "\t location = \"%s\"\n" $loc >> $prefix-$name.tf
-        printf "\t gateway_address = \"%s\"\n" $gwaddr >> $prefix-$name.tf
-        printf "\t address_space = %s\n" "$addrpre" >> $prefix-$name.tf
+        printf "\t type = \"%s\"\n" $type >> $prefix-$name.tf
+        printf "\t vpn_type = \"%s\"\n" $vpntype >> $prefix-$name.tf
+        printf "\t sku = \"%s\"\n" $sku >> $prefix-$name.tf
+        printf "\t active_active = \"%s\"\n" $aa >> $prefix-$name.tf
+        printf "\t enable_bgp = \"%s\"\n" $enbgp >> $prefix-$name.tf
+
     
         if [ "$bgps" != "null" ]; then
+            printf "\t bgp_settings {\n"  >> $prefix-$name.tf
             asn=`echo $azr | jq ".[(${i})].bgpSettings.asn" | tr -d '"'`
             peera=`echo $azr | jq ".[(${i})].bgpSettings.bgpPeeringAddress" | tr -d '"'`
             peerw=`echo $azr | jq ".[(${i})].bgpSettings.peerWeight" | tr -d '"'`
-
-            printf "\t bgp_settings {\n"  >> $prefix-$name.tf
             printf "\t\t asn = \"%s\"\n" $asn >> $prefix-$name.tf
-            printf "\t\t bgp_peering_address = \"%s\"\n" $peera >> $prefix-$name.tf
+            printf "\t\t peering_address = \"%s\"\n" $peera >> $prefix-$name.tf
             printf "\t\t peer_weight = \"%s\"\n" $peerw >> $prefix-$name.tf
             printf "\t }\n"  >> $prefix-$name.tf
         fi
+
+        ipc=`echo $azr | jq ".[(${i})].ipConfigurations"`    
+        count=`echo $ipc | jq '. | length'`
+        count=`expr $count - 1`
+        for j in `seq 0 $count`; do
+            ipcname=`echo $ipc | jq ".[(${j})].name"`
+            ipcpipa=`echo $ipc | jq ".[(${j})].privateIpAllocationMethod"`
+            ipcpipid=`echo $ipc | jq ".[(${j})].publicIpAddress.id"`
+            ipcsubid=`echo $ipc | jq ".[(${j})].subnet.id"`            
+            pipnam=`echo $ipcpipid | cut -d'/' -f9 | tr -d '"'`
+            piprg=`echo $ipcpipid | cut -d'/' -f5 | tr -d '"'`
+            subnam=`echo $ipcsubid | cut -d'/' -f11 | tr -d '"'`
+            subrg=`echo $ipcsubid | cut -d'/' -f5 | tr -d '"'`
+            printf "\tip_configuration {\n"  >> $prefix-$name.tf
+            printf "\t\t name = %s\n" $ipcname >> $prefix-$name.tf
+            printf "\t\t private_ip_address_allocation = %s\n" $ipcpipa >> $prefix-$name.tf
+            if [ "$pipnam" != "null" ]; then
+                printf "\t\t public_ip_address_id = \"\${azurerm_public_ip.%s__%s.id}\"\n" $piprg $pipnam >> $prefix-$name.tf
+            fi
+            if [ "$subnam" != "null" ]; then
+                printf "\t\t subnet_id = \"\${azurerm_subnet.%s__%s.id}\"\n" $subrg $subnam >> $prefix-$name.tf
+            fi
+            printf "\t}\n" >> $prefix-$name.tf          
+        done
 
         #
         # New Tags block
