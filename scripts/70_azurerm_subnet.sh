06241c4d85cc39e538534835bc1768a700bfc5c0
andyt530 <andyt530@outlook.com>
2018-10-21 22:03:39 +0100

    subnet fix for associations

--- scripts/70_azurerm_subnet.sh
+++ scripts/70_azurerm_subnet.sh
@@ -46,14 +46,43 @@ if [ "$count" -gt "0" ]; then
             
             snsg=`echo $azr | jq ".[(${i})].networkSecurityGroup.id" | cut -f9 -d"/" | tr -d '"'`
             snsgrg=`echo $azr | jq ".[(${i})].networkSecurityGroup.id" | cut -f5 -d"/" | tr -d '"'`
-            
+
+            r1="skip"
+            if [ "$snsg" != "null" ]; then
+                r1="azurerm_subnet_network_security_group_association"
+                outsnsg=`printf "%s.%s__%s__%s.tf" $r1 $rg $name $snsg`
+                echo $outsnsg
+                printf "resource \"%s\" \"%s__%s__%s\" {\n" $r1 $rg $name $snsg > $outsnsg
+                printf "\tsubnet_id = \"\${azurerm_subnet.%s__%s.id}\"\n" $rg $name >> $outsnsg
+                printf "\tnetwork_security_group_id = \"\${azurerm_network_security_group.%s__%s.id}\"\n" $snsgrg $snsg >> $outsnsg
+                printf "}\n" >> $outsnsg
+            fi
+
+
             printf "resource \"%s\" \"%s__%s\" {\n" $tfp $rg $name >> $outfile
             printf "\t name = \"%s\"\n" $name >> $outfile
-            
             printf "\t virtual_network_name = \"%s\"\n" $vname >> $outfile
             printf "\t address_prefix = \"%s\"\n" $sprefix >> $outfile
             rtbid=`echo $azr | jq ".[(${i})].routeTable.id" | cut -f9 -d"/" | tr -d '"'`
             rtrg=`echo $azr | jq ".[(${i})].routeTable.id" | cut -f5 -d"/" | tr -d '"'`
+
+# write 
+#  printf "resource "azurerm_subnet_route_table_association" "test" {
+#  subnet_id      = "${azurerm_subnet.test.id}"
+#  printf "\t route_table_id = \"\${azurerm_route_table.%s__%s.id}\"\n" $rtrg $rtbid >> $outfile
+# 
+# }
+            r2="skip"
+            if [ "$rtbid" != "null" ]; then
+                r1="azurerm_subnet_route_table_association"
+                outrtbid=`printf "%s.%s__%s__%s.tf" $r2 $rg $name $snsg`
+                echo $outrtbid
+                printf "resource \"%s\" \"%s__%s__%s\" {\n" $r2 $rg $name $rtbid > $outrtbid
+                printf "\tsubnet_id = \"\${azurerm_subnet.%s__%s.id}\"\n" $rg $name >> $outrtbid
+                printf "\troute_table_id = \"\${azurerm_route_table.%s__%s.id}\"\n" $rtrg $rtbid >> $outrtbid
+                printf "}\n" >> $outrtbid
+            fi
+
             #printf "\t resource_group_name = \"\${var.rgtarget}\"\n" >> $outfile
             printf "\t resource_group_name = \"%s\"\n" $rg >> $outfile
             if [ "$snsg" != "null" ]; then
@@ -67,7 +96,30 @@ if [ "$count" -gt "0" ]; then
             fi
 
             printf "}\n" >> $outfile
+
             cat $outfile
+
+            if [ "$r1" != "skip" ]; then
+            cat $outsnsg
+            statecomm=`printf "terraform state rm %s.%s__%s__%s" $r1 $rg $name $snsg`
+            echo $statecomm >> tf-staterm.sh
+            eval $statecomm
+            evalcomm=`printf "terraform import %s.%s__%s__%s %s" $r1 $rg $name $snsg $id`
+            echo $evalcomm >> tf-stateimp.sh
+            eval $evalcomm
+            fi
+
+#printf "resource \"%s\" \"%s__%s__%s\" {\n" $r2 $rg $name $rtbid > $outrtbid
+            if [ "$r2" != "skip" ]; then
+            cat $outrtbid
+            statecomm=`printf "terraform state rm %s.%s__%s__%s" $r2 $rg $name $rtbid`
+            echo $statecomm >> tf-staterm.sh
+            eval $statecomm
+            evalcomm=`printf "terraform import %s.%s__%s__%s %s" $r2 $rg $name $rtbid $id`
+            echo $evalcomm >> tf-stateimp.sh
+            eval $evalcomm
+            fi
+
             statecomm=`printf "terraform state rm %s.%s__%s" $tfp $rg $name`
             echo $statecomm >> tf-staterm.sh
             eval $statecomm
